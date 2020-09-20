677a112f4ea8c98ee29e4cd065d20df9e5f83f38
Carlos Sepulveda <carlos.sepulvedahe@gmail.com>
2020-09-20 18:23:17 -0300

    base proyecto

---
+++ src/app.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const app = express()
+
+//creamos un servidor http a partir de la libreria de express
+const http = require('http').Server(app);
+
+//para generar una comunicacion vamos a trabajar con socket io
+
+const io = require('socket.io')(http);
+
+app.use(require ('./routes/stream.routes'));
+
+//donde vamos a cargar los html con los que vamos a trabjar 
+
+app.use(express.static(__dirname + "/public" ))
+
+io.on('connection', (socket) => {
+    socket.on('stream', (image)=>{
+        //emitir el evento a todos los sockets conectados
+        socket.broadcast.emit('stream', image);//canal de comunicacion dentro del servidor
+    })
+})
+
+module.exports = http;
+
