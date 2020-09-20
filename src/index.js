2aed1cd9ac029ec4bbcde22b2d26aa63f1d15d6c
Carlos Sepulveda <carlos.sepulvedahe@gmail.com>
2020-09-20 19:17:28 -0300

    fix7

--- src/index.js
+++ src/index.js
@@ -4,20 +4,19 @@ const express = require('express')
 const app = express()
 
 //creamos un servidor http a partir de la libreria de express
-const http = require('http').Server(app);
+const server = http.createServer(app);
 
 //para generar una comunicacion vamos a trabajar con socket io
 
-const io = require('socket.io')(http);
+const io = require('socket.io').listen(server);
 app.set('port', process.env.PORT || 3000);
 app.use(require ('./routes/stream.routes'));
 
 //donde vamos a cargar los html con los que vamos a trabjar 
 
-require('./app')(io);
-
+require('./app')(io)
 app.use(express.static(__dirname + "/public" ))
 
-http.listen(app.get('port') , () => {
+server.listen(app.get('port') , () => {
     console.log('Servidor en el puerto:' + app.get('port'))
 })
