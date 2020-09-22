7145b5f81db3213da4cb8e81812d0f6df7154d13
Carlos Sepulveda <carlos.sepulvedahe@gmail.com>
2020-09-21 21:12:46 -0300

    test deploy dev branch

---
+++ src/public/js/index.js
@@ -0,0 +1,61 @@
+
+
+//--------------------------------------------
+//-------------------DOM----------------------
+//--------------------------------------------
+var canvas = document.querySelector('#preview');
+var context = canvas.getContext('2d')
+var btn = document.querySelector('#btn')
+canvas.style.display = "none";
+
+canvas.width = 512;
+canvas.height = 384;
+
+context.width = canvas.width;
+context.height = canvas.height;
+
+var video = document.querySelector('#video')
+var socket = io();
+
+
+//--------------------------------------------
+//-------------------scripts----------------------
+//--------------------------------------------
+function publicarMensaje(msg){
+    document.querySelector('.status').innerText = msg;
+}
+
+function loadCamara(stream){
+    video.srcObject = stream;
+    publicarMensaje('camara funcionando')
+}
+
+function errorCamara(){
+    publicarMensaje('camara ha fallado')
+}
+
+
+//--------------------------------------------
+//-------------------socket----------------------
+//--------------------------------------------
+function verVideo(vid, con){
+    con.drawImage(vid, 0, 0, con.width, con.height)
+    socket.emit('stream', canvas.toDataURL('image/webp'))
+}
+
+
+
+//--------------------------------------------
+//-------------------EVENTOS----------------------
+//--------------------------------------------
+btn.addEventListener('click', () =>{
+    navigator.getUserMedia = (navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msgGetUserMedia)
+
+    if (navigator.getUserMedia){
+        navigator.getUserMedia({video:true}, loadCamara, errorCamara)
+    }
+
+    var intervalo = setInterval(()=>{
+        verVideo(video, context)
+    },500)
+})
